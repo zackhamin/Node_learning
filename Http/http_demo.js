7a9ba5045ada3830cc1e6f1f1e33efa641866dc6
Ishaq Amin <ishaq.amin@naimuri.com>
2021-03-14 11:11:43 +0000

    webpages as server responses and JSON

---
+++ Http/http_demo.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+//Simple webserver
+
+//Create a server object
+http.createServer((req,res) => {
+    //Write a response
+    res.write('Hello world');
+    res.end();
+}).listen(5000, () => console.log('Server running'));
--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-const Logger = require('./Logger');
+// const Logger = require('./Logger');
 
-const logger = new Logger();
+// const logger = new Logger();
 
-logger.on('message', data => console.log('Called Listener:', data));
+// logger.on('message', data => console.log('Called Listener:', data));
 
-logger.log("Hello world")
+// logger.log("Hello world")
+
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/'){
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,content) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Cotent-Type': 'text/html' });
+            res.end(content)
+        })
+
+
+        // res.end('<h1>home</h1>')
+    }
+    if(req.url === '/about'){
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err,content) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Cotent-Type': 'text/html' });
+            res.end(content)
+        })
+
+
+        // res.end('<h1>home</h1>')
+    }
+
+    if(req.url === '/api/users'){
+      const users = [
+          { name: 'Zack', age:31 },
+          { name: 'Someone else', age:39 }
+      ];
+
+      res.writeHead(200, { 'Cotent-Type': 'application/json' });
+      res.end(JSON.stringify(users))
+        // res.end('<h1>home</h1>')
+    }
+});
+
+const PORT = process.env.port || 5000;
+
+server.listen(5000, () => console.log(`server running: ${PORT}`));
